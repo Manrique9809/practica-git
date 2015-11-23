30a6bb4ee6df2869cd622e0520d2c992e7473c66
Antony Manrique <antony476@gmail.com>
2015-11-23 11:05:37 -0500

    subiendo practica git con sus cdn

--- index.php
+++ index.php
@@ -5,8 +5,31 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<title></title>
 	<link rel="stylesheet" href="">
+	<!-- Latest compiled and minified CSS & JS -->
+	<link rel="stylesheet" media="screen" href="//netdna.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+	<script src="//code.jquery.com/jquery.js"></script>
+	<script src="//netdna.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
 </head>
 <body>
 	<h1>HOLAAAAAA</h1>
+	<label for="input-id" class="col-sm-2"></label>
+	<form action="" method="POST" role="form">
+		<legend>Form title</legend>
+	
+		<div class="form-group">
+			<label for="">label</label>
+			<input type="text" class="form-control" id="" placeholder="Input field">
+			<label for="">label</label>
+			<input type="text" class="form-control" id="" placeholder="Input field">
+			<label for="">label</label>
+			<input type="text" class="form-control" id="" placeholder="Input field">
+			<label for="">label</label>
+			<input type="text" class="form-control" id="" placeholder="Input field">
+		</div>
+	
+		
+	
+		<button type="submit" class="btn btn-primary">Submit</button>
+	</form>
 </body>
 </html>
